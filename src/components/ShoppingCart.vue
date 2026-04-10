486ee9299ab46b9bc2fa975b941733777dbd0768
Thifany2203 <thifanybulgarelli1990@gmail.com>
2026-04-10 20:54:57 -0300

    Sistema atualizado com sucesso

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -2,9 +2,24 @@
   <div>
     <h2>Carrinho de Compras</h2>
 
-    <button @click="addProduct">Adicionar ao carrinho</button>
+    <button @click="addProduct({ id: 1, name: 'Notebook', price: 3500 })">
+      Adicionar Notebook
+    </button>
+
+    <button @click="addProduct({ id: 2, name: 'Mouse', price: 100 })">Adicionar Mouse</button>
+
+    <button @click="addProduct({ id: 3, name: 'Teclado', price: 200 })">Adicionar Teclado</button>
+
+    <ul>
+      <li v-for="(item, index) in $store.state.cart" :key="index">
+        {{ item.name }} - R$ {{ item.price }} (x{{ item.quantity }})
+
+        <button @click="$store.dispatch('removeFromCart', index)">Remover</button>
+      </li>
+    </ul>
 
     <p>Total de itens: {{ $store.getters.cartItemCount }}</p>
+    <p>Total em R$: {{ $store.getters.cartTotalPrice }}</p>
   </div>
 </template>
 
@@ -13,13 +28,7 @@ export default {
   name: 'ShoppingCart',
 
   methods: {
-    addProduct() {
-      const product = {
-        id: 1,
-        name: 'Notebook',
-        price: 3500,
-      }
-
+    addProduct(product) {
       this.$store.dispatch('addToCart', product)
     },
   },
--- src/store/index.js
+++ src/store/index.js
@@ -8,7 +8,23 @@ export default createStore({
   mutations: {
     // Mutations
     ADD_TO_CART(state, product) {
-      state.cart.push(product)
+      const existingProduct = state.cart.find((item) => item.id === product.id)
+
+      if (existingProduct) {
+        existingProduct.quantity++
+      } else {
+        state.cart.push({ ...product, quantity: 1 })
+      }
+    },
+
+    REMOVE_FROM_CART(state, index) {
+      const item = state.cart[index]
+
+      if (item.quantity > 1) {
+        item.quantity--
+      } else {
+        state.cart.splice(index, 1)
+      }
     },
   },
   actions: {
@@ -16,9 +32,18 @@ export default createStore({
     addToCart({ commit }, product) {
       commit('ADD_TO_CART', product)
     },
+
+    removeFromCart({ commit }, index) {
+      commit('REMOVE_FROM_CART', index)
+    },
   },
   getters: {
     // Getters
     cartItemCount: (state) => state.cart.length,
+    cartTotalPrice: (state) => {
+      return state.cart.reduce((total, item) => {
+        return total + item.price * item.quantity
+      }, 0)
+    },
   },
 })
